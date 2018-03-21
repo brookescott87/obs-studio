da86291c69350df1dd4a6bb90596ca54f9f9233f
blipvert <blipvert@blipvertica.com>
2018-03-21 16:47:21 -0400

    Updated for VS 2017

--- buildinit.cmd
+++ buildinit.cmd
@@ -7,7 +7,7 @@ set VERSION=%VERSION:-=                              %
 set VERSION=%VERSION:~0,30%
 set VERSION=%VERSION: =%
 
-set OBS_INSTALL_PREFIX="F:/Program Files/OBS Studio/%VERSION%"
+set OBS_INSTALL_PREFIX="%OBS_ROOT%/%VERSION%"
 
 if exist %OBS_INSTALL_PREFIX% goto args
 echo Directory "%OBS_INSTALL_PREFIX" does not exist.
@@ -18,8 +18,9 @@ goto done
 
 if "%1"=="2013" goto vs2013
 if "%1"=="2015" goto vs2015
+if "%1"=="2017" goto vs2017
 
-echo Need to specify either 2013 or 2015
+echo Need to specify one of 2013, 2015, or 2017
 goto done
 
 :vs2013
@@ -33,6 +34,12 @@ set DepsPath=%HOMEDRIVE%%HOMEPATH%/Library/obs-studio-dependencies/2015/include
 set QTDIR=c:/Qt/Qt5.7.0/5.7/msvc2015_64
 set cmgen="Visual Studio 14 2015 Win64"
 goto common
+
+:vs2017
+set DepsPath=%OBS_ROOT%/dependencies/2015/win64/include
+set QTDIR=D:/Qt/5.10.1/msvc2017_64
+set cmgen="Visual Studio 15 2017 Win64"
+
 
 :common
 echo Building for OBS Studio %VERSION%.
